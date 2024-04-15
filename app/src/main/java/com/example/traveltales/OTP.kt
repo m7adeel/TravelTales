a44c0911c3f65cc22509406c9d1b3608b176f481
Muhammad Adeel <m7adeel@gmail.com>
2024-04-15 16:38:41 +0500

    add: all screens layout

--- app/src/main/java/com/example/traveltales/OTP.kt
+++ app/src/main/java/com/example/traveltales/OTP.kt
@@ -3,11 +3,9 @@ package com.example.traveltales
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class OTP : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-
-        // Splash Screen
+        setContentView(R.layout.activity_otp)
     }
 }
