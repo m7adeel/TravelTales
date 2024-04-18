d1f76375117c5202a8c5aaf0db75ef68c1a9654a
Muhammad Adeel <m7adeel@gmail.com>
2024-04-18 23:23:02 +0500

    add: xml layouts

--- app/src/main/java/com/example/traveltales/MainActivity.kt
+++ app/src/main/java/com/example/traveltales/MainActivity.kt
@@ -1,13 +1,30 @@
 package com.example.traveltales
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.util.Log
+import android.widget.Button
+import android.widget.EditText
+import com.android.volley.Request
+import com.android.volley.Response
+import com.android.volley.toolbox.StringRequest
+import com.android.volley.toolbox.Volley
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        // Splash Screen
+        val intent = Intent(this, GetStarted::class.java)
+
+        // Start your next activity after 3000ms
+        Handler().postDelayed({
+            startActivity(intent)
+            finish()
+        }, 3000)
+
+
     }
 }
